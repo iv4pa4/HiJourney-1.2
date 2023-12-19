29765411d8ffae10979045bc13c75b6db287f8db
Ivayla Panayotova <99187068+iv4pa4@users.noreply.github.com>
2023-12-19 10:04:44 +0200

    Fixing missing preview content

---
+++ HiJourney 1.2/HiJourney 1.2/HiJourney_1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HiJourney_1_2App.swift
+//  HiJourney 1.2
+//
+//  Created by Ivayla  Panayotova on 19.12.23.
+//
+
+import SwiftUI
+
+@main
+struct HiJourney_1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
