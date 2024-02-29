40180afd9e4881b0c45659b28306639c8ecd4fe5
Ivayla Panayotova <99187068+iv4pa4@users.noreply.github.com>
2024-02-29 11:41:03 +0200

    fixing important varables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,41 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdventurerModule } from './adventurer/adventurer.module';
 import { CreatorModule } from './creator/creator.module';
 import { AdventureModule } from './adventure/adventure.module';
-//import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AdventurerModule,
     CreatorModule,
     AdventureModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'iva',
-      password: 'iva2005iva2005',
-      database: 'hijourney',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      } as TypeOrmModuleOptions),
     }),
-    JwtModule.register({
-      secret: 'your-secret-key', // Replace with your own secret key
-      signOptions: { expiresIn: '1h' }, // Optionally, specify token expiration
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
